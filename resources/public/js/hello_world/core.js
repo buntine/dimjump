9c476d16459cc2aac3b997ef6e99e445a222ce2b
Andrew Buntine <info@bunts.io>
2018-08-28 15:46:59 +1000

    Removed JS directory from VCS

--- resources/public/js/hello_world/core.js
+++
@@ -1,6 +0,0 @@
-// Compiled by ClojureScript 1.10.339 {}
-goog.provide('hello_world.core');
-goog.require('cljs.core');
-cljs.core.println.call(null,"Hello world!");
-
-//# sourceMappingURL=core.js.map?rel=1535420667357
