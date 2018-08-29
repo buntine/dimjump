6919262de60a8cdf4474a50f8b9dc11b4ee12759
Andrew Buntine <info@bunts.io>
2018-08-29 10:56:37 +1000

    Added correct width/height calculations

--- src/cljs/dimjump/core.cljs
+++ src/cljs/dimjump/core.cljs
@@ -2,6 +2,9 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
+(def dimensions {:w (* 0.8 (.-innerWidth js/window))
+                 :h 360})
+
 (defn setup []
   (q/frame-rate 10)
   (q/no-stroke)
@@ -10,7 +13,10 @@
 (defn draw-ground []
   (q/with-fill
     [197 226 175]
-    (q/rect 0 200 500 200))) ; TODO: Calcuate correctly: 25% of "height", 100% of width. Positioned at 75% from top.
+    (q/rect 0
+            (* 0.75 (:h dimensions))
+            (:w dimensions)
+            (* 0.25 (:h dimensions)))))
 
 (defn draw [state]
   (q/background (q/color 176 214 255))
@@ -28,5 +34,8 @@
     :settings #(q/smooth 2)
     :setup setup
     :draw draw
+    :size (vals dimensions)
     :update progress
     :middleware [m/fun-mode]))
+
+(init)
