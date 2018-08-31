e00b7904a0965e1c9e1ab3d0fadce3e229365b6f
Andrew Buntine Of Doom <bunts@hhd.com.au>
2018-08-31 16:56:51 +1000

    Rendering of arbitrary polygons

--- src/cljs/dimjump/data.cljs
+++ src/cljs/dimjump/data.cljs
@@ -1,14 +1,32 @@
 (ns dimjump.data)
 
+(defn rect [x w h offset]
+  "Returns set of vertices for given rectangle"
+  [[x (- (+ h offset))]
+   [(+ x w) (- (+ h offset))]
+   [(+ x w) (- offset)]
+   [x (- offset)]])
+
 (defn block
   ([x w h] (block x w h 0))
-  ([x w h offset]
-    {:x x :w w :h h :offset offset}))
+  ([x w h offset] (block x w h 0 (rect x w h 0)))
+  ([x w h offset vertices]
+    {:x x :w w :h h :offset offset :vertices vertices}))
 
 (def levels
   [[(block 200 20 20)
-    (block 300 60 5 17)
-    (block 400 20 20)]
-   [(block 300 20 20)
+    (block 400 20 30)
+    (block 600 30 20)]
+   [(block 160 20 20)
+    (block 360 20 20)
     (block 500 20 20)
-    (block 600 20 20)]])
+    (block 660 100 10 27)]
+   [(block 120 20 20)
+    (block 300 20 20)
+    (block 400 20 20)
+    (block 520 20 30)
+    (block 700 35 15)]
+  ])
+
+; Example:
+; (block 340  40 25 0 [[340 -18] [350 -18] [350 -25] [370 -25] [370 -18] [380 -18] [380 0] [340 0]])
--- src/cljs/dimjump/obstacle.cljs
+++ src/cljs/dimjump/obstacle.cljs
@@ -1,13 +1,13 @@
 (ns dimjump.obstacle
   (:require [quil.core :as q :include-macros true]))
 
-(defn draw [{x :x w :w h :h offset :offset} floor-y]
+(defn draw [{x :x w :w h :h vertices :vertices} floor-y]
   (q/with-fill
     [178 145 116]
-    (q/rect x
-      (- floor-y h offset)
-      w
-      h)))
+    (q/begin-shape)
+    (doseq [[x y] vertices]
+      (q/vertex x (+ floor-y y)))
+    (q/end-shape)))
 
 (defn collision? [floor-y {px :x py :y pw :w ph :h} {ox :x ow :w oh :h offset :offset}]
   "Returns true if any obstacle in the level has collided
