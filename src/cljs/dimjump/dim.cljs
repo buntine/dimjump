d616df8a00da88dce410989d0b8a1fb5c29d1ee2
Andrew Buntine <info@bunts.io>
2018-09-04 00:12:44 +1000

    Added trail to dim - needs refactor

--- src/cljs/dimjump/dim.cljs
+++ src/cljs/dimjump/dim.cljs
@@ -3,8 +3,7 @@
              [dimjump.sound :as sound]))
 
 (defn spawn [y]
-  {:trail 5
-   :points (take 5 (repeat {:x -20 :y y}))
+  {:points (take 5 (repeat {:x -20 :y y}))
    :w 16
    :h 24
    :v 0
@@ -36,16 +35,13 @@
            (select-keys dim [:w :h]))))
 
 (defn add-point 
-  ([dim {x :x y :y}]
-    (add-point dim x y))
   ([dim x y]
-    (update dim :points (comp rest conj) {:x x :y y})))
-
-(defn add-point-y [dim y]
-  (add-point dim (:x dim) y))
+    (add-point dim {:x x :y y}))
+  ([dim point]
+    (update dim :points (comp vec rest conj) point)))
 
 (defn add-point-x [dim x]
-  (add-point dim x (:y dim)))
+  (add-point dim x (:y (position dim))))
 
 (defn reset [dim]
   (add-point-x dim -20))
@@ -68,11 +64,13 @@
           toggle-jump
           (assoc :v velocity)))))
 
-;; REMOVE?
 (defn end-jump [dim y]
-  (-> dim
-      toggle-jump
-      (add-point-y y)))
+  (if (and (:jumping dim)
+           (= (:y (position dim)) y))
+    (-> dim
+        toggle-jump
+        (assoc :v 0))
+    dim))
 
 (defn kill [dim]
   (-> dim
@@ -87,25 +85,12 @@
 
 (defn progress-jump [dim floor-y]
   "Returns the next Y position for the dim during a jump"
-  (let [next-y (+ (:y dim) (:v dim))]
-    (if (:jumping dim)
-      (if (> next-y floor-y)
-        floor-y
-        next-y)
-      (:y dim))))
+  (let [y (:y (position dim))
+        next-y (+ y (:v dim))]
+    (min floor-y next-y)))
 
-(defn next-point [dim floor-y]
-  "Returns the next point at which the player should appear"
-  (let [next-x (+ (:x dim) (:speed dim))
-        next-y (progress-jump dim floor-y)]
-    {:x next-x
-     :y next-y}))
-
-(defn reposition [dim floor-y gravity]
-  (-> dim
-      (update-velocity gravity)
-      (next-point floor-y)
-      add-point))
+(defn accellerate [dim]
+  (+ (:x (position dim)) (:speed dim)))
 
 (defn next-level [dim]
   (-> dim
@@ -114,19 +99,26 @@
 
 (defn progress-level [dim width]
   "Checks if it's necessary to go to the next level."
-  (let [last-point (last (:points dim))]
-    (if (>= (:x last-point) width)
+  (let [pos (position dim)]
+    (if (>= (:x pos) width)
       (next-level dim)
       dim)))
 
 (defn progress [state]
   "Receives full game state and returns next state. Coupling is permitted
    here."
-  (assoc state
+  (let [dim (:dim state)
+        floor-y (:floor-y state)
+        gravity (:gravity state)
+        next-x (accellerate dim)
+        next-y (progress-jump dim floor-y)]
+    (assoc state
          :dim
-         (-> (:dim state)
-             (reposition (:floor-y state) (:gravity state))
-             (progress-level (:w state)))))
+         (-> dim
+             (add-point next-x next-y)
+             (progress-velocity gravity)
+             (end-jump floor-y)
+             (progress-level (:w state))))))
 
 (defn sprite-for [frame dim]
   "Returns the PImage suitable for the given frame number"
@@ -134,11 +126,23 @@
         frames ((if (:ducking dim) :ducking :standing) all-frames)]
     (frames (mod (int (/ frame (:animation-speed dim))) 2))))
 
+(defn trail-opacities
+  "A lazy sequence that produces the opacities"
+  ([] (trail-opacities 255))
+  ([n] (let [next-n (if (= n 255) 60 (- n 10))]
+         (lazy-seq 
+           (cons n (trail-opacities next-n))))))
+
 (defn draw [dim frame-number]
   "Receives player state and renders"
-  (let [sprite (sprite-for frame-number dim)]
-    (let [{x :x y :y} (last (:points dim))]
-    (q/image sprite
-             x
-             (- y (.-height sprite)))
-    )))
+  (let [sprite (sprite-for frame-number dim)
+        points (reverse (:points dim))
+        trail (take (count points) (trail-opacities))
+        trail-with-opacities (map-indexed #(vector %2 (nth trail %1))
+                                          points)]
+    (doseq [[{x :x y :y} opacity] trail-with-opacities]
+      (q/tint 255 opacity)
+      (q/image sprite
+               x
+               (- y (.-height sprite)))
+      (q/no-tint))))
