ea59910f9be49601fbf4f32f804b9b8d08032252
Andrew Buntine <info@bunts.io>
2018-09-06 15:45:08 +1000

    Simplified corpse rendering

--- src/cljs/dimjump/corpse.cljs
+++ src/cljs/dimjump/corpse.cljs
@@ -11,15 +11,13 @@
    :alpha 255})
 
 (defn draw [{x :x y :y h :h rotation :rotation sprite :sprite alpha :alpha}]
-  (let [distance-from-pi (Math/abs (- Math/PI rotation))]
-    (q/push-matrix)
-    (q/tint 255 alpha)
-    (q/translate (- x 0)
-                 (- y (* h (/ distance-from-pi Math/PI))))
-    (q/rotate rotation)
-    (q/image sprite 0 0)
-    (q/no-tint)
-    (q/pop-matrix)))
+  (q/push-matrix)
+  (q/image-mode :center)
+  (q/tint 255 alpha)
+  (q/translate x y)
+  (q/rotate rotation)
+  (q/image sprite 0 0)
+  (q/pop-matrix))
 
 (defn progress [corpse]
   (update corpse :alpha - (:degradation corpse)))
