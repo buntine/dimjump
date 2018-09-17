f15a280c2d91f4822328997f6854b97662dc29ef
Andrew Buntine <info@bunts.io>
2018-09-17 22:35:03 +1000

    Added options map as final argument to block

--- src/cljs/dimjump/data.cljs
+++ src/cljs/dimjump/data.cljs
@@ -9,10 +9,10 @@
    [x (- offset)]])
 
 (defn block
-  ([x w h] (block x w h 0))
-  ([x w h offset] (block x w h offset (rect x w h offset)))
-  ([x w h offset vertices]
-    {:x x :w w :h h :offset offset :vertices vertices}))
+  ([x w h] (block x w h {:offset 0}))
+  ([x w h {:keys [offset] :or {offset 0} :as opts}] (block x w h (rect x w h offset) opts))
+  ([x w h vertices opts]
+    (merge opts {:x x :w w :h h :vertices vertices})))
 
 (def text
   (memoize (fn [size]
@@ -36,35 +36,35 @@
    [(block 160 20 20)
     (block 360 20 20)
     (block 500 20 20)
-    (block 660 100 10 27)]
+    (block 660 100 10 {:offset 27})]
    [(block 120 20 20)
     (block 300 20 20)
     (block 400 20 20)
     (block 520 20 30)
     (block 700 35 15)]
    [(block 115 8 8)
-    (block 123 8 8 8)
-    (block 131 8 8 16)
-    (block 139 8 8 8)
+    (block 123 8 8 {:offset 8})
+    (block 131 8 8 {:offset 16})
+    (block 139 8 8 {:offset 8})
     (block 147 8 8)
     (block 220 37 9)
     (block 350 25 25)
-    (block 420 90 7 27)
-    (block 510 100 7 17)
-    (block 610 90 7 27)
+    (block 420 90 7 {:offset 27})
+    (block 510 100 7 {:offset 17})
+    (block 610 90 7 {:offset 27})
     (block 740 20 20)]
    [(block 110 20 20)
     (block 200 20 20)
     (block 290 20 20)
-    (block 350 100 5 27)
+    (block 350 100 5 {:offset 27})
     (block 490 35 12)
     (block 600 20 40)
     (block 740 20 30)]
-   [(block 120 140 8 17)
+   [(block 120 140 8 {:offset 17})
     (block 325 32 31)
-    (block 420 200 12 27)
+    (block 420 200 12 {:offset 27})
     (block 646 20 20)]
-   [(block 120 500 5 27)
+   [(block 120 500 5 {:offset 27})
     (block 660 30 20)
     (block 760 30 20)]
    [(block 120 20 20)
