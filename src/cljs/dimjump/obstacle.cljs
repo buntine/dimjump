9f29339ab5ceed4662af8a0db4e38c82bb72d856
Andrew Buntine <info@bunts.io>
2023-05-11 21:17:39 +1000

    Blindly copied draw routine to obstacle and exit. Fight me

--- src/cljs/dimjump/obstacle.cljs
+++ src/cljs/dimjump/obstacle.cljs
@@ -19,22 +19,31 @@
   [id x y w h min-x max-y min-y speed move-x move-y fade-cycle background]
   quadrangle/Quadrangle
 
-  (draw [{:keys [fade-cycle background]}]
+  (draw [{:keys [fade-cycle background bounce?]}]
     (let [{:keys [alpha]
            :or {alpha 255}} fade-cycle
-          {:keys [frames]} background
+          {:keys [frames layers rotation]} background
           canvas (.getElementById js/document  "game")
           ctx (.getContext canvas "2d")
-          img (.getElementById js/document (quadrangle/image-for frames))
-          pattern (.createPattern ctx img "repeat")]
+          bg-img (.getElementById js/document (quadrangle/image-for frames))
+          pattern (.createPattern ctx bg-img "repeat")
+          matrix (.rotate (js/DOMMatrix. [1 0 0 1 0 0]) rotation)
+          y-pos (- y h)]
+
+      (.setTransform pattern matrix)
 
       (set! (.-fillStyle ctx) pattern)
       (set! (.-globalAlpha ctx) (/ alpha 255))
 
-      (.beginPath ctx)
-      (.rect ctx x (- y h) w h)
-      (.closePath ctx)
-      (.fill ctx)
+      (quadrangle/draw-rect ctx x y-pos w h)
+
+      (doseq [{img :img l-y :y} layers]
+        (let [l-img (.getElementById js/document img)
+              l-pattern (.createPattern ctx l-img "repeat-x")]
+          (.setTransform pattern matrix)
+          (set! (.-fillStyle ctx) l-pattern)
+
+          (quadrangle/draw-rect ctx x (+ y-pos l-y) w h)))
 
       (set! (.-globalAlpha ctx) 255)))
 
