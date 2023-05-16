d4898e848dbe2416f321c6a8a9b93379a0bf1a3b
Andrew Buntine <info@bunts.io>
2023-05-16 22:25:33 +1000

    Render gameport nicer

--- src/clj/dimjump/views.clj
+++ src/clj/dimjump/views.clj
@@ -16,13 +16,14 @@
       [:span {:class "one"} "."]
       [:span {:class "two"} "."]
       [:span {:class "three"} "."]]
-    [:img {:id "brick" :src "/images/brick.png"}]
-    [:img {:id "oilspill_1" :src "/images/oilspill_1.png"}]
-    [:img {:id "oilspill_2" :src "/images/oilspill_2.png"}]
-    [:img {:id "spikes_1" :src "/images/spikes_1.png"}]
-    [:img {:id "rock_1" :src "/images/rock_1.png"}]
-    [:img {:id "exit_sign_1" :src "/images/exit_sign_1.png"}]
-    [:img {:id "trampoline_1" :src "/images/trampoline_1.png"}]
+    [:div {:id "sprites"}
+      [:img {:id "brick" :src "/images/brick.png"}]
+      [:img {:id "oilspill_1" :src "/images/oilspill_1.png"}]
+      [:img {:id "oilspill_2" :src "/images/oilspill_2.png"}]
+      [:img {:id "spikes_1" :src "/images/spikes_1.png"}]
+      [:img {:id "rock_1" :src "/images/rock_1.png"}]
+      [:img {:id "exit_sign_1" :src "/images/exit_sign_1.png"}]
+      [:img {:id "trampoline_1" :src "/images/trampoline_1.png"}]]
     [:canvas {:id "game"}]])
 
 (defn homepage []
