d86813c400a7f15746f68563a60903852dfae899
Andrew Buntine <info@bunts.io>
2024-07-01 14:47:51 +0700

    chore: Updated deps

--- project.clj
+++ project.clj
@@ -2,19 +2,18 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [compojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [compojure "1.7.0"]
                  [ring/ring-defaults "0.2.1"]
                  [hiccup "1.0.5"]
-                 [org.clojure/clojurescript "1.10.339"]
+                 [org.clojure/clojurescript "1.11.60"]
                  [org.clojure/java.jdbc "0.6.0"]
                  [quil "2.7.1"]
                  [com.h2database/h2 "1.4.193"]]
   :plugins [[lein-ring "0.12.6"]
             [lein-figwheel "0.5.20"]]
   :cljsbuild {:builds [{:source-paths ["src/cljs"]
-                        :figwheel {:websocket-host "0.0.0.0"
-                                   :on-jsload "dimjump.core/init"}
+                        :figwheel true
                         :id "dev"
                         :compiler {:optimizations :none
                                    :main "dimjump.core"
